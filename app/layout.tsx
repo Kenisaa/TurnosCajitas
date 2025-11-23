e42f2393b7c5c7838adc7a959d0fe400ac54eeb9
Kenneth Callejas <kcallejas24@hotmail.com>
2025-11-23 14:59:59 -0600

    Cambio nombre pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Turno cajitas',
+  description: 'Asamblea de circuito 2025-2026',
   generator: 'v0.app',
   icons: {
     icon: [
